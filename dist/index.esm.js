4bd0fe34a908f78febcb57fc5e19e60da96cd5e0
zoia.baletskaya <zoia.baletskaya@zensoftware.nl>
2023-03-23 12:14:45 +0100

    including dist to the repository

---
+++ dist/index.esm.js
@@ -0,0 +1,4 @@
+export { A as AgileAnalyticsPage, d as agileAnalyticsPlugin } from './esm/index-087680fe.esm.js';
+import '@backstage/core-plugin-api';
+import 'moment';
+//# sourceMappingURL=index.esm.js.map
