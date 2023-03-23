12e90927beaf074ae642ac6421fe15172069e046
zoia.baletskaya <zoia.baletskaya@zensoftware.nl>
2023-03-23 10:33:27 +0100

    removes dist

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-import * as _backstage_core_plugin_api from '@backstage/core-plugin-api';
-
-declare const agileAnalyticsPlugin: _backstage_core_plugin_api.BackstagePlugin<{
-    root: _backstage_core_plugin_api.RouteRef<undefined>;
-}, {}>;
-declare const AgileAnalyticsPage: () => JSX.Element;
-
-export { AgileAnalyticsPage, agileAnalyticsPlugin };
--- dist/index.esm.js
+++
@@ -1,4 +0,0 @@
-export { A as AgileAnalyticsPage, d as agileAnalyticsPlugin } from './esm/index-087680fe.esm.js';
-import '@backstage/core-plugin-api';
-import 'moment';
-//# sourceMappingURL=index.esm.js.map
