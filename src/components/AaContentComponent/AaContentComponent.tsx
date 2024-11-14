a4871e447d91ad0b5c332f7510fa99e6fecbe578
zoia.baletskaya <zoia.baletskaya@zensoftware.nl>
2024-11-14 12:41:13 +0200

    updates permissions

--- src/components/AaContentComponent/AaContentComponent.tsx
+++ src/components/AaContentComponent/AaContentComponent.tsx
@@ -1,19 +1,20 @@
 /* eslint-disable no-console */
-import React, { useState } from 'react';
-import { Grid } from '@material-ui/core';
+import React, { useState } from "react";
+import { Grid } from "@material-ui/core";
 import {
   Content,
   InfoCard,
   StructuredMetadataTable,
   Tabs,
-} from '@backstage/core-components';
-import { OrganisationDataResponse } from '../../api/types';
-import { AaTimeSelect } from '../AaTimeSelect';
-import { getEndDate, getStartDate } from '../../helpers';
-import { AaDoraPage } from '../AaDoraPage';
-import { AaSprintInsightsPage } from '../AaSprintInsightsPage';
-import { AaStockPage } from '../AaStockPage';
-import { AaLeaksPage } from '../AaLeaksPage';
+} from "@backstage/core-components";
+import { OrganisationDataResponse } from "../../api/types";
+import { AaTimeSelect } from "../AaTimeSelect";
+import { getEndDate, getStartDate } from "../../helpers";
+import { AaDoraPage } from "../AaDoraPage";
+import { AaSprintInsightsPage } from "../AaSprintInsightsPage";
+import { AaStockPage } from "../AaStockPage";
+import { AaLeaksPage } from "../AaLeaksPage";
+import { Alert } from "@material-ui/lab";
 
 export const AaContentComponent = ({
   orgData,
@@ -21,16 +22,16 @@ export const AaContentComponent = ({
   orgData: OrganisationDataResponse;
 }) => {
   const [timeperiod, setTimeperiod] = useState({
-    date_start: getStartDate(6, 'days'),
+    date_start: getStartDate(6, "days"),
     date_end: getEndDate(),
-    label: 'Last 7 days',
-    value: '7days',
+    label: "Last 7 days",
+    value: "7days",
   });
 
   const overviewMetadata = {
-    'Organisation hash': orgData.orgHash,
-    'Organisation name': orgData.orgName,
-    'Number of users': orgData.usersNumber,
+    "Organisation hash": orgData.orgHash,
+    "Organisation name": orgData.orgName,
+    "Number of users": orgData.usersNumber,
     Status: orgData.status,
     Subscription: orgData.subscription,
   };
@@ -38,7 +39,7 @@ export const AaContentComponent = ({
   const cardContentStyle = { heightX: 200, width: 600 };
   const tabs = [
     {
-      label: 'OVERVIEW',
+      label: "OVERVIEW",
       content: (
         <Grid container spacing={3} direction="column" style={cardContentStyle}>
           <Grid item>
@@ -50,46 +51,83 @@ export const AaContentComponent = ({
       ),
     },
     {
-      label: 'SPRINT INSIGHTS',
+      label: "SPRINT INSIGHTS",
       content: (
         <Grid container spacing={3} direction="column">
           <Grid item>
-            <AaSprintInsightsPage timeperiod={timeperiod} />
+            {orgData?.subscription === "enterprise-plus" ? (
+              <AaSprintInsightsPage
+                timeperiod={timeperiod}
+                plan={orgData.subscription}
+              />
+            ) : (
+              renderUpgradeWarning()
+            )}
           </Grid>
         </Grid>
       ),
     },
     {
-      label: 'DORA',
+      label: "DORA",
       content: (
         <Grid container spacing={3} direction="column">
           <Grid item>
-            <AaDoraPage timeperiod={timeperiod} />
+            {orgData?.subscription === "enterprise-plus" ? (
+              <AaDoraPage timeperiod={timeperiod} plan={orgData.subscription} />
+            ) : (
+              renderUpgradeWarning()
+            )}
           </Grid>
         </Grid>
       ),
     },
     {
-      label: 'STOCK',
+      label: "STOCK",
       content: (
         <Grid container spacing={3} direction="column">
           <Grid item>
-            <AaStockPage timeperiod={timeperiod} />
+            {orgData?.subscription === "enterprise-plus" ? (
+              <AaStockPage
+                timeperiod={timeperiod}
+                plan={orgData.subscription}
+              />
+            ) : (
+              renderUpgradeWarning()
+            )}
           </Grid>
         </Grid>
       ),
     },
     {
-      label: 'LEAKS',
+      label: "LEAKS",
       content: (
         <Grid container spacing={3} direction="column">
           <Grid item>
-            <AaLeaksPage timeperiod={timeperiod} />
+            {orgData?.subscription === "enterprise-plus" ? (
+              <AaLeaksPage
+                timeperiod={timeperiod}
+                plan={orgData.subscription}
+              />
+            ) : (
+              renderUpgradeWarning()
+            )}
           </Grid>
         </Grid>
       ),
     },
   ];
+
+  function renderUpgradeWarning() {
+    return (
+      <Alert severity="warning">
+        Agile Analytics Backstage.io intagration available only for Enterprise+
+        organisations.{" "}
+        <a href="https://www.prod.agileanalytics.cloud/settings/organisation">
+          Upgrade your plan
+        </a>
+      </Alert>
+    );
+  }
 
   return (
     <Content>
--- src/components/AaStockPage/AaStockPage.tsx
+++ src/components/AaStockPage/AaStockPage.tsx
@@ -1,20 +1,26 @@
 /* eslint-disable no-console */
-import React from 'react';
-import moment from 'moment';
-import { GaugeCard, InfoCard, Progress } from '@backstage/core-components';
-import { configApiRef, useApi } from '@backstage/core-plugin-api';
-import useAsync from 'react-use/lib/useAsync';
-import Alert from '@material-ui/lab/Alert';
-import { Grid } from '@material-ui/core';
-import { Timeperiod } from '../../api/types';
-import { agileAnalyticsApiRef } from '../../api';
-import { AaDoraChart } from '../AaDoraChart';
+import React from "react";
+import moment from "moment";
+import { GaugeCard, InfoCard, Progress } from "@backstage/core-components";
+import { configApiRef, useApi } from "@backstage/core-plugin-api";
+import useAsync from "react-use/lib/useAsync";
+import Alert from "@material-ui/lab/Alert";
+import { Grid } from "@material-ui/core";
+import { Timeperiod } from "../../api/types";
+import { agileAnalyticsApiRef } from "../../api";
+import { AaDoraChart } from "../AaDoraChart";
 
-export const AaStockPage = ({ timeperiod }: { timeperiod: Timeperiod }) => {
+export const AaStockPage = ({
+  timeperiod,
+  plan,
+}: {
+  timeperiod: Timeperiod;
+  plan: string;
+}) => {
   const api = useApi(agileAnalyticsApiRef);
   const config = useApi(configApiRef);
-  const orgHash = config.getString('agileAnalytics.orgHash');
-  const apiKey = config.getString('agileAnalytics.apiKey');
+  const orgHash = config.getString("agileAnalytics.orgHash");
+  const apiKey = config.getString("agileAnalytics.apiKey");
 
   const stockState = useAsync(async (): Promise<any> => {
     const response = await api.getStockData({
@@ -49,7 +55,7 @@ export const AaStockPage = ({ timeperiod }: { timeperiod: Timeperiod }) => {
       if (!timestamps.length) {
         return [];
       }
-      timestamps.forEach(item => {
+      timestamps.forEach((item) => {
         const start_date = moment(item).unix();
         labels.push(start_date);
       });
@@ -59,7 +65,7 @@ export const AaStockPage = ({ timeperiod }: { timeperiod: Timeperiod }) => {
 
     const series = stockState?.value
       ? Object.keys(stockState?.value)
-          .filter(item => item !== 'buckets')
+          .filter((item) => item !== "buckets")
           .reduce((acc: { name: string; data: number[][] }[], item) => {
             const formatted = {
               name: item,
@@ -67,7 +73,7 @@ export const AaStockPage = ({ timeperiod }: { timeperiod: Timeperiod }) => {
                 date * 1000,
                 stockState.value[item][i]?.avg_branch_total_modified_lines,
               ]),
-              type: 'column',
+              type: "column",
             };
             return [...acc, formatted];
           }, [])
@@ -82,11 +88,11 @@ export const AaStockPage = ({ timeperiod }: { timeperiod: Timeperiod }) => {
     }
     const series = [
       {
-        name: '',
-        data: Object.entries(riskChartState?.value).map(entry => {
+        name: "",
+        data: Object.entries(riskChartState?.value).map((entry) => {
           return [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]];
         }),
-        type: 'pie',
+        type: "pie",
       },
     ];
     return series;
@@ -118,19 +124,19 @@ export const AaStockPage = ({ timeperiod }: { timeperiod: Timeperiod }) => {
             timeperiod={timeperiod}
             charts={chartOptions}
             chartColor={[
-              '#15a2bb',
-              '#54afc4',
-              '#79bccd',
-              '#99c9d6',
-              '#b7d6df',
-              '#d4e3e8',
-              '#f1f1f1',
-              '#f7dbde',
-              '#fcc5cb',
-              '#ffaeb9',
-              '#ff97a7',
-              '#ff7e95',
-              '#ff6384',
+              "#15a2bb",
+              "#54afc4",
+              "#79bccd",
+              "#99c9d6",
+              "#b7d6df",
+              "#d4e3e8",
+              "#f1f1f1",
+              "#f7dbde",
+              "#fcc5cb",
+              "#ffaeb9",
+              "#ff97a7",
+              "#ff7e95",
+              "#ff6384",
             ]}
             // chartHeight={360}
             loading={stockState.loading}
@@ -146,8 +152,8 @@ export const AaStockPage = ({ timeperiod }: { timeperiod: Timeperiod }) => {
         <InfoCard
           title="Risk Chart"
           deepLink={{
-            title: 'Go to Agile Analytics to see a detailed report  ',
-            link: 'https://www.prod.agileanalytics.cloud/stock',
+            title: "Go to Agile Analytics to see a detailed report  ",
+            link: "https://www.prod.agileanalytics.cloud/stock",
           }}
         >
           {riskChartState.error ? (
@@ -156,7 +162,7 @@ export const AaStockPage = ({ timeperiod }: { timeperiod: Timeperiod }) => {
           <AaDoraChart
             timeperiod={timeperiod}
             charts={riskChartOptions}
-            chartColor={['#92CE52', '#F8C238', '#E11E1E']}
+            chartColor={["#92CE52", "#F8C238", "#E11E1E"]}
             loading={riskChartState.loading}
             customPointFormatter={function (this: any) {
               return `<span>Branches amount: <b>${this.options.y}</b></span>`;
